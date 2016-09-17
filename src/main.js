1a23ff8a3e7fc001be1e74f800c0e967540f53b3
Vijay Patel <vijay@spiritedtechie.com>
2016-09-17 09:17:43 +0100

    Recreated base skeleton project for ES6 with JSPM & SystemJS.

--- src/main.js
+++ src/main.js
@@ -1,25 +1,16 @@
-import environment from './environment';
-
-//Configure Bluebird Promises.
-//Note: You may want to use environment-specific configuration.
-Promise.config({
-  warnings: {
-    wForgottenReturn: false
-  }
-});
+import 'bootstrap';
 
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
-    .feature('resources');
+    .developmentLogging();
 
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
+  //Uncomment the line below to enable animation.
+  //aurelia.use.plugin('aurelia-animator-css');
+  //if the css animator is enabled, add swap-order="after" to all router-view elements
 
-  if (environment.testing) {
-    aurelia.use.plugin('aurelia-testing');
-  }
+  //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+  //aurelia.use.plugin('aurelia-html-import-template-loader')
 
   aurelia.start().then(() => aurelia.setRoot());
 }
