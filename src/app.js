482a471b25d9df3664be24d5a16f8c6d104c207d
Vijay Patel <vijay@spiritedtechie.com>
2016-09-18 20:53:51 +0100

    Experimenting with home page hero banner

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
+      { route: ['', 'home'], name: 'home',      moduleId: 'home',      nav: true, title: 'Home' },
       { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
     ]);
 
---
+++ src/home.js
@@ -0,0 +1,9 @@
+//import {computedFrom} from 'aurelia-framework';
+
+export class Home {
+  heading = 'FOURSQUARE';
+  subheadingFirst = "Find the best places to eat, drink, shop, or visit in any city in the world.";
+  subheadingSecond = "Access over 75 million short tips from local experts.";
+  findHint = "I'm looking for..."
+  location = "London"
+}
