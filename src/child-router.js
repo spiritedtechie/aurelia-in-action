b0e51d1ca7d7ab0c0138e77072329c2c331b6ca9
Vijay Patel <vijay@spiritedtechie.com>
2016-09-24 13:04:46 +0100

    Few more tweaks to css and positioning

--- src/child-router.js
+++ src/child-router.js
@@ -3,8 +3,6 @@ export class ChildRouter {
 
   configureRouter(config, router) {
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',       moduleId: 'welcome',       nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',         moduleId: 'users',         nav: true, title: 'Github Users' },
       { route: 'child-router',  name: 'child-router',  moduleId: 'child-router',  nav: true, title: 'Child Router' }
     ]);
 
