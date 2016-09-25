35b73227056a83fdfdff58409e5057c4240b1769
Vijay Patel <vijay@spiritedtechie.com>
2016-09-25 18:54:47 +0100

    Added child router for place categories

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'home'], name: 'home',      moduleId: 'home',      nav: true, title: 'Home' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: '', redirect: 'home' },
+      { route: 'home', name: 'home',      moduleId: 'home',      nav: true, title: 'Home' }
     ]);
 
     this.router = router;
--- src/home.js
+++ src/home.js
@@ -4,6 +4,18 @@ export class Home {
   heading = 'FOURSQUARE';
   subheadingFirst = "Find the best places to eat, drink, shop, or visit in any city in the world.";
   subheadingSecond = "Access over 75 million short tips from local experts.";
-  findHint = "I'm looking for..."
-  location = "London"
+  findHint = "I'm looking for...";
+  location = "London";
+
+  configureRouter(config, router) {
+    config.map([
+      { route: ['', 'food'],  name: 'food',  moduleId: 'food/food',  nav: true, title: 'Food' },
+      { route: 'coffee',  name: 'coffee',  moduleId: 'coffee/coffee',  nav: true, title: 'Coffee' },
+      { route: 'nightlife',  name: 'nightlife',  moduleId: 'nightlife/nightlife',  nav: true, title: 'Nightlife' },
+      { route: 'fun',  name: 'fun',  moduleId: 'fun/fun',  nav: true, title: 'Fun' },
+      { route: 'shopping',  name: 'shopping',  moduleId: 'shopping/shopping',  nav: true, title: 'Shopping' }
+    ]);
+
+    this.router = router;
+  }
 }
